8560044bc751cea13e30f19ecef2d1888beba363
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-10-18 08:30:17 -0400

    import dependencies and add basic information

---
+++ realtime-graph/app.py
@@ -0,0 +1,24 @@
+#app.py
+from flask import Flask, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from pusher import Pusher
+import requests, json, atexit, time, plotly, plotly.graph_objs as go
+
+app = Flask(__name__)
+
+# configure pusher object
+pusher = Pusher(
+        app_id='625209',
+        key='d0fbb6934c4ec3d8a001',
+        secret='d1471059e40c19e1a1fe',
+        cluster='us2',
+        ssl=True
+)
+
+#Array of times
+times = []
+#The name of currencies stored in an array
+currencies = ["BTC"]
+#Prices and their prices stored in a array in a dictionary.
+prices = {"BTC": []}
