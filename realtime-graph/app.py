c3c00ec36ae0f84e865476ffd19bf9ef21ea05bb
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-10-18 09:15:50 -0400

    create retrieve_data function

--- realtime-graph/app.py
+++ realtime-graph/app.py
@@ -27,3 +27,60 @@ prices = {"BTC": []}
 @app.route("/")
 def index():
     return render_template("index.html")
+
+def retrieve_data():
+    #Create a dictionary of current Prices
+    current_prices = {}
+    #For each currency in currencies, initialize each with an empty array as key
+    for currency in currencies:
+        current_prices[currency] = []
+        ##Debug - print current_prices dictionary
+        print(current_prices)
+
+    times.append(time.strftime('%H:%M:%S'))
+
+    #Make an API request and get back the object
+    api_url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms=USD".format(",".join(currencies))
+    #Get amount of the currency
+    response = json.loads(requests.get(api_url).content)
+
+    for currency in currencies:
+        price = response[currency]['USD']
+        current_prices[currency] = price
+        prices[currency].append(price)
+
+    graph_data = [go.Scatter(
+        x=times,
+        y=prices.get(currency),
+        name="{} Prices".format(currency)
+    ) for current in currencies]
+
+    # create an array of traces for bar chart data
+    bar_chart_data = [go.Bar(
+        x=currencies,
+        y=list(current_prices.values())
+    )]
+
+    data = {
+        'graph': json.dumps(list(graph_data), cls=plotly.utils.PlotlyJSONEcoder),
+        'bar_chart': json.dumps(list(bar_chart_data), cls=plotly.utils.PlotlyJSONEcoder)
+    }
+
+    #Trigger pusher event
+    pusher.trigger("crypto", "data-updated", data)
+
+    # create schedule for retrieving prices
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    scheduler.add_job(
+        func=retrieve_data,
+        trigger = IntervalTrigger(seconds=10),
+        id='prices_retrieval_job',
+        name='Retrieve prices every 10 seconds',
+        replace_existing = True
+    )
+
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
+
+    app.run(debug=True, use_reloader=False)
