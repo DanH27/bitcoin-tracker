57e282dc8948829e1df2aff742e0d9354f916873
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-11-24 18:23:18 -0500

    Add login, register, and logout functions

--- realtime-graph/bitcointracker/__init__.py
+++ realtime-graph/bitcointracker/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'f9fc238959f76fa032a7294936fcd953'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from bitcointracker import routes
--- realtime-graph/bitcointracker/routes.py
+++ realtime-graph/bitcointracker/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from bitcointracker import app
+from bitcointracker import app, db, bcrypt
 from bitcointracker.forms import RegistrationForm, LoginForm
 from bitcointracker.models import User, Currency
 from pusher import Pusher
@@ -7,7 +7,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from flask import request
 import requests, json, atexit, time, plotly, plotly.graph_objs as go
-
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -247,15 +247,44 @@ def btc_dash():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Youur account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Login Uncessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title="Account")
+
+
 
 @app.route("/currencies", methods=['POST', 'GET'])
 def ctable():
