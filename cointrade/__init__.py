158d62107ea049afd0e5a448b418cd18ecce52fa
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-12-11 15:26:26 -0500

    Ready to deploy on heroku

--- cointrade/__init__.py
+++ cointrade/__init__.py
@@ -12,10 +12,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f9fc238959f76fa032a7294936fcd953'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-if debugging:
-     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/bitrade'
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://teopiqfwhkgadm:b39ab93fd66a7c02bf68417cc1dc62d67ed9fb1b68b51939908978c2f12bc11a@ec2-54-163-245-64.compute-1.amazonaws.com:5432/d72h6r5qn3sd0'
+#if debugging:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/bitrade'
+#else:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://teopiqfwhkgadm:b39ab93fd66a7c02bf68417cc1dc62d67ed9fb1b68b51939908978c2f12bc11a@ec2-54-163-245-64.compute-1.amazonaws.com:5432/d72h6r5qn3sd0'
 #Make a db object using SQLAlchemy
 db = SQLAlchemy(app)
 #Make a bcrypt object
